d7da1e3302b8a240c69198eb23d590927e0dce64
Jacks113 <59602950+Jacks113@users.noreply.github.com>
2022-03-08 14:40:03 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import AddTask from './Components/AddTask';
+import ShowTasks from './Components/ShowTasks';
+
+
+function App() {
+  return (
+    <div className="App">
+      <AddTask/>
+      <ShowTasks/>
+    </div>
+  );
+}
+
+export default App;
